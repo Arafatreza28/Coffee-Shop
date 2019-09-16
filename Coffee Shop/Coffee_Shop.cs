c1876d37ef3d2e7d6ba3bdd10b5e99fca3724344
Arafatreza28 <arafatrezabitm3@gmail.com>
2019-09-16 21:03:59 +0600

    Coffee Shop With array

---
+++ Coffee Shop/Coffee_Shop.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coffee_Shop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string[] customerName = new string[100];
+            string[] contactNo = new string[100];
+            string[] Address = new string[100];
+            string[] Order = new string[100];
+            string[] Quantity = new string[100];
+            int[] quantity= new int[100];
+            int[] price = new int[100];
+            int i = 0;
+
+
+            if (customernameTextBox.Text == "" || contactnoTextBox.Text == "" || addressTextBox.Text == "" ||
+                orderComboBox.Text == "" || quantityTextBox.Text == "")
+            {
+                if (customernameTextBox.Text == "" && contactnoTextBox.Text == "" && addressTextBox.Text == "" &&
+                    orderComboBox.Text == "" && quantityTextBox.Text == "")
+                {
+                    MessageBox.Show("Please enter all customer information");
+                }
+                else if (customernameTextBox.Text == "")
+                {
+                    MessageBox.Show("Please enter a customer name");
+                }
+                else if (contactnoTextBox.Text == "")
+                {
+                    MessageBox.Show("Please enter a customer no");
+                }
+                else if (addressTextBox.Text == "")
+                {
+                    MessageBox.Show("Please enter a address");
+                }
+                else if (orderComboBox.Text == "")
+                {
+                    MessageBox.Show("Please enter a order");
+                }
+                else if (quantityTextBox.Text == "")
+                {
+                    MessageBox.Show("Please enter a quantity");
+                }
+                else
+                {
+                    MessageBox.Show("Please enter all customer information");
+                }
+
+                
+            }
+            else
+            {
+                customerName[i] = customernameTextBox.Text;
+                contactNo[i] = contactnoTextBox.Text;
+                Address[i] = addressTextBox.Text;
+                Order[i] = orderComboBox.Text;
+                Quantity[i] = quantityTextBox.Text;
+
+                quantity[i] = Convert.ToInt32(Quantity[i]);
+
+                if (orderComboBox.Text == "Black")
+                {
+                    price[i] = quantity[i] * 120;
+                    MessageBox.Show("The bill is " + price[i] + "Tk.");
+                }
+                else if (orderComboBox.Text == "Cold")
+                {
+                    price[i] = quantity[i] * 100;
+                    MessageBox.Show("The bill is " + price[i] + "Tk.");
+                }
+                else if (orderComboBox.Text == "Hot")
+                {
+                    price[i] = quantity[i] * 90;
+                    MessageBox.Show("The bill is " + price[i] + "Tk.");
+                }
+                else if (orderComboBox.Text == "Regular")
+                {
+                    price[i] = quantity[i] * 80;
+                    MessageBox.Show("The bill is " + price[i] + "Tk.");
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a order");
+                }
+                
+
+                purchase_informationRichTextBox.SelectedText = Environment.NewLine + "Customer Name :" + customerName[i];
+                purchase_informationRichTextBox.SelectedText = Environment.NewLine + "Contact Number:" + contactNo[i];
+                purchase_informationRichTextBox.SelectedText = Environment.NewLine + "Address             :" + Address[i];
+                purchase_informationRichTextBox.SelectedText = Environment.NewLine + "Order                 :" + Order[i];
+                purchase_informationRichTextBox.SelectedText = Environment.NewLine + "Quantity            :" + quantity[i];
+                purchase_informationRichTextBox.SelectedText = Environment.NewLine + "Price                 :" + price[i] + "Tk.";
+                purchase_informationRichTextBox.SelectedText = Environment.NewLine + "";
+
+                MessageBox.Show("Successfully Saved");
+
+                customernameTextBox.Text = "";
+                contactnoTextBox.Text = "";
+                addressTextBox.Text = "";
+                orderComboBox.Text = "";
+                quantityTextBox.Text = "";
+            }
+
+        }
+    }
+}
